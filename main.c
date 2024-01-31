4899159324e2022c4727cd69d782454778e5597e
gabafo1 <gabrielafolabi@gmail.com>
2024-01-31 19:46:07 +0100

    clock

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    int h, m, s;
+    int d = 1000;
+    printf("Set time: \n");
+
+    scanf("%d%d%d",&h,&m,&s);
+    if(h > 12 || m > 60 || s>60){
+        printf("ERROR! \n");
+        exit(0);
+    }
+    while(1){
+        s++;
+        if(s>59){
+            m++;
+            s = 0;
+        }
+        if(h>59){
+            h++;
+            m = 0;
+        }
+        if(m>12){
+            h = 0;
+        }
+        printf("\n Clock:");
+        printf("%02d:%02d:%02d",h,m,s);
+        Sleep(d);
+        system("cls");
+    }
+    return 0;
+}
